479e573f68acf06e50f6768de3895d751100cfff
joangnz <joangnz@gmail.com>
2025-12-09 15:51:55 +0100

    Further improvement on TilePainter Code

--- Assets/Scripts/TilePainter.cs
+++ Assets/Scripts/TilePainter.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Tilemaps;
 
@@ -6,6 +8,7 @@ public class TilePainter : MonoBehaviour
 {
     private Tilemap Fg, Deco;
     private Tile TLCorner, TRCorner, BLCorner, BRCorner, TEdge, LEdge, REdge, BEdge, Fill, Ramp;
+    private Dictionary<string, Tile> tileDict;
     private int lastCol, y, x;
     public void Init(Tilemap fg, Tilemap deco, Tile tlCorner, Tile trCorner, Tile blCorner, Tile brCorner, Tile tEdge, Tile lEdge, Tile rEdge, Tile bEdge, Tile fill, Tile ramp)
     {
@@ -21,9 +24,23 @@ public class TilePainter : MonoBehaviour
         BEdge = bEdge;
         Fill = fill;
         Ramp = ramp;
+
+        tileDict = new()
+        {
+            {"TLCorner", TLCorner },
+            {"TRCorner", TRCorner },
+            {"BLCorner", BLCorner },
+            {"BRCorner", BRCorner },
+            {"TEdge", TEdge },
+            {"LEdge", LEdge },
+            {"REdge", REdge },
+            {"BEdge", BEdge },
+            {"Fill", Fill },
+            {"Ramp", Ramp }
+        };
     }
 
-    private Vector2 GetStartPos(Vector2 playerPos)
+    private Vector2Int GetStartPos(Vector2 playerPos)
     {
         Vector2 pos = playerPos;
 
@@ -33,14 +50,14 @@ public class TilePainter : MonoBehaviour
         {
             pos.y *= -1;
         }
-        return pos;
+        return Vector2Int.RoundToInt(pos);
     }
 
     public void RandomPaint(Vector2 playerPos)
     {
         int rows = -Random.Range(2, 11);
         int cols = Random.Range(4, 18);
-        Vector2 startPos = GetStartPos(playerPos);
+        Vector2Int startPos = GetStartPos(playerPos);
 
         lastCol = cols-1;
 
@@ -48,32 +65,34 @@ public class TilePainter : MonoBehaviour
         {
             for (x = 0; x < cols; x++)
             {
+                Vector2Int curPos = new(startPos.x + x, startPos.y + y);
+
                 // First Row, First Column
-                if (y==0 && x==0) PaintTileOrRamp("TLCorner", new(0, 0));
+                if (y==0 && x==0) PaintTileOrRamp("TLCorner", curPos);
 
                 // First Row, Last Column
-                else if (y==0 && x==lastCol) PaintTile("BRCorner", new(0, 0));
+                else if (y==0 && x==lastCol) PaintTile("BRCorner", curPos);
 
                 // First Row, Mid Columns
-                else if (y==0 && x<lastCol) PaintTileOrRamp("TEdge", new(0, 0));
+                else if (y==0 && x<lastCol) PaintTileOrRamp("TEdge", curPos);
 
                 // Last Row, First Column
-                else if (y==rows+1 && x==0) PaintTile("BLCorner", new(0, 0));
+                else if (y==rows+1 && x==0) PaintTile("BLCorner", curPos);
 
                 // Last Row, Last Column
-                else if (y==rows+1 && x==lastCol) PaintTile("BRCorner", new(0, 0));
+                else if (y==rows+1 && x==lastCol) PaintTile("BRCorner", curPos);
 
                 // Last Row, Mid Columns
-                else if (y==rows+1 && x<lastCol) PaintTile("BEdge", new(0, 0));
+                else if (y==rows+1 && x<lastCol) PaintTile("BEdge", curPos);
 
                 // Mid Rows, First Column
-                else if (y<0 && x==0) PaintTile("LEdge", new(0, 0));
+                else if (y<0 && x==0) PaintTile("LEdge", curPos);
 
                 // Mid Rows, Last Column
-                else if (y<0 && x==lastCol) PaintTileOrRamp("REdge", new(0, 0));
+                else if (y<0 && x==lastCol) PaintTileOrRamp("REdge", curPos);
 
                 // Mid Rows, Mid Columns
-                else PaintTile("Fill", new(0, 0));
+                else PaintTile("Fill", curPos);
             }
         }
     }
@@ -91,6 +110,6 @@ public class TilePainter : MonoBehaviour
 
     private void PaintTile(string tName, Vector2Int pos)
     {
-
+        Fg.SetTile((Vector3Int)pos, tileDict[tName]);
     }
 }
